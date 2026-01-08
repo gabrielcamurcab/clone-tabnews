152e6689cc4cad4e53be181b900fdcc6f78f92b2
Gabriel Camurça Bezerra <122840209+gabrielcamurcab@users.noreply.github.com>
2026-01-08 19:47:30 +0000

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
 function Home() {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100vh",
-      backgroundColor: "#f5f5f5",
-      fontFamily: "sans-serif"
-    }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#f5f5f5",
+        fontFamily: "sans-serif",
+      }}
+    >
       <h1 style={{ fontSize: "2rem", marginBottom: "10px" }}>Em construção</h1>
-      <p style={{ fontSize: "1.1rem", color: "#555" }}>Aqui será um ambiente para que você, candidato, gerencie todas as suas candidaturas a emprego.</p>
+      <p style={{ fontSize: "1.1rem", color: "#555" }}>
+        Eu não sei o que terá, aqui, sinceramente kk. Mas o projeto tá ficando
+        robusto, e minha senioridade tá subindo!
+      </p>
     </div>
   );
 }
