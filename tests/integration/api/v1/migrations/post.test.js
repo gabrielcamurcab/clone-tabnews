36f5962444579a8d1c4adb8b0f87631a5c220b47
Gabriel Camurça Bezerra <122840209+gabrielcamurcab@users.noreply.github.com>
2026-02-09 18:54:15 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,31 +6,44 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
+
+        const response1Body = await response1.json();
+
+        const countMigrations = response1Body.length;
+
+        const testQuery = await database.query(
+          "SELECT count(*) count FROM pg_migrations",
+        );
+        const count = parseInt(testQuery.rows[0].count);
+
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(count).toBe(countMigrations);
+      });
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const response1Body = await response1.json();
-
-  const countMigrations = response1Body.length;
-
-  const testQuery = await database.query(
-    "SELECT count(*) count FROM pg_migrations",
-  );
-  const count = parseInt(testQuery.rows[0].count);
-
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(count).toBe(countMigrations);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
 });
